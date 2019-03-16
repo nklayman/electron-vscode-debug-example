f945e90b19ac0dae6c13041c04f5b981a4770314
Noah Klayman <noahklayman@gmail.com>
2019-03-15 18:38:56 -0700

    fix linting error

--- src/background.js
+++ src/background.js
@@ -58,6 +58,7 @@ app.on('ready', async () => {
     try {
       await installVueDevtools()
     } catch (e) {
+      // eslint-disable-next-line no-console
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
